687976ba0fb527075cd18cafac159fb0fdb38a7e
ToxicStar8 <64957222+ToxicStar8@users.noreply.github.com>
2026-09-01 11:55:18 +0800

    去掉海钓和云冠群岛的屏蔽，避免副本内重叠

--- BlockAnyRace/Plugin.cs
+++ BlockAnyRace/Plugin.cs
@@ -69,9 +69,9 @@ namespace Main
                     //TerritoryIntendedUse=区域预期用途
                     //0=城镇，1=野外，13=住宅区室外
                     //19=旧陆行鸟广场，21=苍穹街，23=金碟游乐场
-                    //44=金碟游乐场跳跳乐，46=海钓，47=云冠群岛
+                    //44=金碟游乐场跳跳乐
                     //幻境村有1269（用途15）和1278（用途7）两个区域，仅按区域编号放行，避免包含其它事件区域
-                    x => (x.TerritoryIntendedUse.Value.RowId is 0 or 1 or 13 or 19 or 21 or 23 or 44 or 46 or 47 ||
+                    x => (x.TerritoryIntendedUse.Value.RowId is 0 or 1 or 13 or 19 or 21 or 23 or 44 ||
                           x.RowId is 1269 or 1278) &&
                           !string.IsNullOrEmpty(x.Name.ToString()) && 
                           x.RowId is not 136).Select(
